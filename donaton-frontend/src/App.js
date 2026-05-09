c1f2090bd74e2a796115c38bce881c621da4f07d
Amaro19 <fraa.guzman@duocuc.cl>
2026-05-09 19:20:39 -0400

    frontend casi listo

---
+++ donaton-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Bienvenido a Donaton</h1>
+        <p>Aplicación de gestión de donaciones</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
